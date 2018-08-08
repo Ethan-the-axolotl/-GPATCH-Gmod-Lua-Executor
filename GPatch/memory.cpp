fef7cb038573cad049d785088b50ab184363e79a
Juxstin <Juxstin>
2018-08-08 03:08:07 -0500

    Source Uploaded
    Source Uploaded

---
+++ GPatch/memory.cpp
@@ -0,0 +1,17 @@
+#include <memory.h>
+#include <cstdint>
+#include "retcheck.h"
+#include <stdio.h>    
+#include <psapi.h>
+#include "stdafx.h"
+#pragma comment( lib, "psapi.lib" )
+
+/*
+Eternals retcheck
+Eternals retcheck is fine but it skips some parts...
+*/
+namespace Retcheck
+{
+	
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # -GPATCH-Gmod-Lua-Executor
 Gmod Lua Executor
+
+No addresses needed(So every update you can use without hassle!)
+
+Bypasses most AC's
